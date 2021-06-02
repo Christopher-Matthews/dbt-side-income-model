cff7a7126fbbb05fb9bee383ae196712ba8753a4
Christopher Matthews <chrismatthews@Christophers-MacBook-Air.local>
2021-06-01 23:53:28 -0700

    add date spine and skeleton for marts structure

---
+++ side_income/models/marts/core/core_dim_date.sql
@@ -0,0 +1,35 @@
+
+with date_spine as ( 
+  select 
+    date_day
+  from 
+    unnest(generate_date_array('2015-12-23', '2016-01-09')) as date_day
+),
+
+add_weekday as (
+  select
+    date_day
+    , case 
+        when extract(dayofweek from date_day) = 1 
+          then 'Sunday'
+        when extract(dayofweek from date_day) = 2
+          then 'Monday'
+        when extract(dayofweek from date_day) = 3
+          then 'Tuesday'
+        when extract(dayofweek from date_day) = 4
+          then 'Wednesday'
+        when extract(dayofweek from date_day) = 5
+          then 'Thursday'
+        when extract(dayofweek from date_day) = 6
+          then 'Friday'
+        when extract(dayofweek from date_day) = 7
+          then 'Saturday'
+      end as day_of_week
+  from 
+    date_spine
+)
+
+select 
+  *
+from 
+  add_weekday
