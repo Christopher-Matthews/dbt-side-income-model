39b366d3e3236b4a30df62dc2912a80fa701efd9
Christopher Matthews <chrismatthews@Christophers-MacBook-Air.local>
2021-06-06 10:27:13 -0700

    add total running portfolio marts table for both arcc and spyd

---
+++ side_income/models/marts/portfolio/portfolio_running_arcc_position.sql
@@ -0,0 +1,40 @@
+with prices as (
+  select
+    *
+  from
+    {{ ref('int_portfolio_arcc_price') }}
+),
+
+cumulative_holdings as (
+  select 
+      date_day
+    , running_quantity_owned
+  from 
+    {{ ref('int_portfolio_cum_arcc_holding') }}
+),
+
+total_position as (
+  select 
+    prices.date_day
+    , prices.day_of_week
+    , prices.ticker_symbol
+    , round(
+        coalesce(
+          prices.close_price * cumulative_holdings.running_quantity_owned
+          , 0)
+        , 2
+      ) as total_running_position
+  from 
+    prices 
+  left join 
+    cumulative_holdings on 
+      prices.date_day = cumulative_holdings.date_day
+  order by 
+    prices.date_day
+)
+
+select 
+  *
+from 
+  total_position
+  
